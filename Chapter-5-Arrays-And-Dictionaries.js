6a0b40d0e3cfe82425b97425b95b283e86e2b33c
Chris Mitchell <chrisjmit@gmail.com>
2017-08-06 17:33:40 +0100

    Adds item 46

--- Chapter-5-Arrays-And-Dictionaries.js
+++ Chapter-5-Arrays-And-Dictionaries.js
@@ -56,3 +56,20 @@ var dict = {};
 "chris" in dict; //false
 "toString" in dict; //true
 "valueOf" in dict; //true
+
+//Item 46: Prefer Arrays to Dictionaries for Ordered Collections
+
+function report(highScores) {
+  var result = "";
+  for (var i = 0, n = highScores.length; i < n; i++) {
+    var score = highScores[i];
+    result += (i + 1) + ". " +
+              score.name + ": " + score.points + "\n";
+  }
+  return result;
+}
+
+report([{ name: "Hank", points: 1110100 },
+        { name: "Steve", points: 1064500 },
+        { name: "Billy", points: 1050200 }]);
+// "1. Hank: 1110100\n2. Steve: 1064500\n3. Billy: 1050200\n"
